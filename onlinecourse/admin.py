814f42a5be0922cae1bfb5ff19e8777742e406b1
Elecbound <elecboy2@hotmail.com>
2022-01-30 03:02:31 -0500

    Terminal crashing ~ woohoo.

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,21 +1,28 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
-
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
 
 class LessonInline(admin.StackedInline):
     model = Lesson
-    extra = 5
+
+class QuestionInline(admin.StackedInline):
+    model = Lesson
+    
+class ChoiceInline(admin.StackedInline):
+    model = Lesson
 
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline], [QuestionInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
+
 
 
 class LessonAdmin(admin.ModelAdmin):
@@ -28,3 +35,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
